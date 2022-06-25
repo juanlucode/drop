6705a2dd9c7b3daafea67f80fea9d76931933b03
juanluis.garciar <juanluis.garciar@juntaex.es>
2022-06-25 13:29:50 +0200

    Loading the Assets

---
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.badlogic.drop.desktop;
+
+import com.badlogic.drop.Drop;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+
+    public static void main(String[] arg){
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Drop");
+        config.setWindowedMode(800, 480);
+        config.useVsync(true);
+        config.setForegroundFPS(60);
+        new Lwjgl3Application(new Drop(), config);
+    }
+}
